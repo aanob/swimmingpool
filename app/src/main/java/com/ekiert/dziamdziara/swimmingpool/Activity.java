f2e7096d2f9443e5fb1ffa89822c2c758dcb7360
Dziamdziara <ekiert4@gmail.com>
2017-06-04 13:46:47 +0200

    naprawianie błędów

--- app/src/main/java/com/ekiert/dziamdziara/swimmingpool/Activity.java
+++ app/src/main/java/com/ekiert/dziamdziara/swimmingpool/Activity.java
@@ -1,31 +1,18 @@
 package com.ekiert.dziamdziara.swimmingpool;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
 
 
-public class Activity extends AppCompatActivity implements OnMapReadyCallback {
-
-    GoogleMap mGoogleMap;
+public class Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -56,51 +43,6 @@ public class Activity extends AppCompatActivity implements OnMapReadyCallback {
 
             }
         });
-        if(googleServicesAvailable()) {
-            Toast.makeText(this, "Polaczono!", Toast.LENGTH_SHORT).show();
-            setContentView(R.layout.activity_main);
-            initMap();
-        }else{
-
-        }
-    }
-
-    private void initMap() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
-        mapFragment.getMapAsync(this);
-    }
-
-    public boolean googleServicesAvailable()
-    {
-        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
-        int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if(isAvailable == ConnectionResult.SUCCESS) {
-            return true;
-        } else if (api.isUserResolvableError(isAvailable)){
-            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
-            dialog.show();
-        } else {
-            Toast.makeText(this, "Nie polaczono z play servisev", Toast.LENGTH_SHORT).show();
-        }
-        return false;
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-        goToLocationZoom(50.652463, 17.905598, 15);
-    }
-
-    private void goToLocation(double lat, double lng) {
-        LatLng ll = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
-        mGoogleMap.moveCamera(update);
-    }
-
-    private void goToLocationZoom(double lat, double lng, float zoom) {
-        LatLng ll = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
-        mGoogleMap.moveCamera(update);
     }
 
 //    @Override
